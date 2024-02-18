1dbe98b334287cdc4695a198ca22f5fb104fb0c5
KunalL08 <82529432+KunalL08@users.noreply.github.com>
2024-02-18 13:47:03 +0000

    Worked on data type conversions in JS

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,99 @@
+//Conversion of different data types into numbers
+
+// const a = "33";
+// const c = "33ab";
+// const e = null;
+// const g = undefined;
+// const i = true;
+
+// console.log(typeof a);
+// console.log(typeof c);
+// console.log(typeof e);
+// console.log(typeof g);
+// console.log(typeof i);
+
+// const b = Number(a);
+
+// console.log(typeof b);
+// console.log(b);
+
+
+// const d = Number(c);
+
+// console.log(typeof  d);
+// console.log(d);
+
+
+// const f = Number(e);
+
+// console.log(typeof f);
+// console.log(f)
+
+
+// const h = Number(g);
+
+// console.log(typeof h);
+// console.log(h);
+
+// const j= Number(i);
+
+// console.log(typeof j);
+// console.log(j);
+
+
+
+
+// conversion of data types to boolean
+
+// const isNumber = 1;
+
+// const isBooleanFromNumber = Boolean(isNumber);
+// console.log(typeof isBooleanFromNumber)
+// console.log(isBooleanFromNumber);
+
+
+// const isString = "hitesh";
+
+// const isBooleanFromString = Boolean(isString);
+// console.log(typeof isBooleanFromString);
+// console.log(isBooleanFromString);
+
+// const isBlankString = "";
+
+// const isBooleanFromBlank = Boolean(isBlankString);
+// console.log(typeof isBooleanFromString);
+// console.log(isBooleanFromBlank);
+
+// const isNumberOtherThan1And0 = 33;
+
+// const isBooleanFromNumberOtherThan1And0 = Boolean(isNumberOtherThan1And0);
+// console.log(typeof isBooleanFromNumberOtherThan1And0);
+// console.log(isBooleanFromNumberOtherThan1And0);
+
+
+
+//conversion of data type to String
+
+// const isNumber = 1;
+
+// const isNumberToString = String(isNumber);
+// console.log(typeof isNumberToString);
+// console.log(isNumberToString);
+
+// const isBoolean = true;
+
+// const isBooleanToString = String(isBoolean);
+// console.log(typeof isBooleanToString);
+// console.log(isBooleanToString);
+
+// const isNull = null;
+
+// const isNullToString = String(isNull);
+// console.log(typeof isNullToString);
+// console.log(isNullToString);
+
+// const isUndefined = undefined;
+
+// const isUndfinedtoString = String(isUndefined);
+// console.log(typeof isUndfinedtoString);
+// console.log(isUndfinedtoString);
