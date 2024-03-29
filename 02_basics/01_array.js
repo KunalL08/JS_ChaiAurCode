233ad17b347431c3b497f82cbcee45c2c38d1768
KunalL08 <82529432+KunalL08@users.noreply.github.com>
2024-03-29 12:29:17 +0000

    "Array basics 01"

---
+++ 02_basics/01_array.js
@@ -0,0 +1,10 @@
+// declaration 
+
+let newArray = [1,2,3,4,5];
+let newArray2 = new Array(1,2,3,4,5);
+
+// we have various methods in array's
+// newArray2.push(2); // pushes 2 in the last
+// newArray2.pop(); //pops out last element 
+// newArray2.unshift(9); //adds the 9 to start of array shifts arrray elements furthur
+// newArray2.shift(); //pops the first element from the array
